be605c1a81e8c543a8b2e3b74fe3be21fcf33129
Johan Andersson <johan.andersson@yourdolphin.com>
2014-09-19 10:41:17 +0200

    Removed "Tricky words"

--- src/com/example/reader/interfaces/OnTrickyWordListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.reader.interfaces;
-
-public interface OnTrickyWordListener {
-	void onTrickyWord(Boolean success);
-}
--- src/com/example/reader/popups/WordActivity.java
+++ src/com/example/reader/popups/WordActivity.java
@@ -1,23 +1,15 @@
 package com.example.reader.popups;
 
 import ilearnrw.textclassification.Word;
-import ilearnrw.user.profile.UserProfile;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import com.example.reader.R;
 import com.example.reader.ReaderActivity;
-import com.example.reader.interfaces.OnHttpListener;
-import com.example.reader.interfaces.OnProfileFetched;
 import com.example.reader.interfaces.OnTextToSpeechComplete;
-import com.example.reader.interfaces.OnTrickyWordListener;
-import com.example.reader.tasks.ProfileTask;
-import com.example.reader.tasks.TrickyWordsTask;
 import com.example.reader.texttospeech.TextToSpeechBase;
 import com.example.reader.types.WordPopupAdapter;
-import com.example.reader.utils.HttpHelper;
-import com.google.gson.Gson;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -30,7 +22,6 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextUtils;
 import android.text.style.BackgroundColorSpan;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
@@ -39,24 +30,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class WordActivity 
 	extends 
 		Activity 
 	implements
-		OnTextToSpeechComplete,
-		OnHttpListener,
-		OnTrickyWordListener,
-		OnProfileFetched {
-
-	
-	private final String TAG = getClass().getName();
+		OnTextToSpeechComplete {
 	
 	private TextView tvTitle;
 	private ImageView ivSpeak;
 	private ListView list;
-	private Button btnAddTrickyWord, btnOk;
+	private Button btnOk;
 	private TextToSpeechBase tts;
 	
 	private SharedPreferences sp;
@@ -65,8 +49,6 @@ public class WordActivity
 	private Word currentWord;
 	private String strWord;
 	private int currentIndex;
-	private Boolean isAdding;
-	private Boolean isFetchingProfile;
 	
 	
 	@SuppressWarnings("unchecked")
@@ -84,7 +66,6 @@ public class WordActivity
 		tvTitle 			= (TextView) findViewById(R.id.tv_word_title);
 		ivSpeak 			= (ImageView) findViewById(R.id.iv_word_speak);
 		list 				= (ListView) findViewById(R.id.lv_word_info);
-		btnAddTrickyWord 	= (Button) findViewById(R.id.btn_add_tricky_word);
 		btnOk				= (Button) findViewById(R.id.btn_word_ok);
 		
 		String def = getResources().getString(R.string.default_text);
@@ -115,19 +96,6 @@ public class WordActivity
 			}
 		});
 		ivSpeak.setEnabled(false);
-		
-		btnAddTrickyWord.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if(currentWord == null){
-					isAdding = true;
-					new TrickyWordsTask(WordActivity.this, true, WordActivity.this, WordActivity.this).run(Integer.toString(id), strWord, token);
-				} else {
-					isAdding = false;
-					new TrickyWordsTask(WordActivity.this, false, WordActivity.this, WordActivity.this).run(Integer.toString(id), strWord, token);
-				}
-			}
-		});
 		
 		btnOk.setOnClickListener(new OnClickListener() {
 			@Override
@@ -173,8 +141,6 @@ public class WordActivity
 		
 		ArrayAdapter<String> adapter = new WordPopupAdapter(this, R.layout.row_word_popup, objects, items, true);
 		list.setAdapter(adapter);
-		
-		isFetchingProfile = false;
 	}
 
 
@@ -212,10 +178,7 @@ public class WordActivity
 
 	private void setWord(String word){
 		currentWord 	= null;
-		currentIndex 	= -1;
-		isAdding 		= true;
-		btnAddTrickyWord.setText(getResources().getString(R.string.add_tricky_word));
-		
+		currentIndex 	= -1;		
 		
 		for(int i=0; i<trickyWords.size(); i++){
 			Word w = trickyWords.get(i);
@@ -225,87 +188,9 @@ public class WordActivity
 			if(w.getWord().equals(word)){
 				currentWord 	= w;
 				currentIndex 	= i;
-				isAdding		= false;
-				btnAddTrickyWord.setText(getResources().getString(R.string.remove_tricky_word));
 				break;
 			}
 		}
 		
-	}
-
-	@Override
-	public void onTokenExpired(final String... params) {
-		if(HttpHelper.refreshTokens(this)){
-			final String newToken = PreferenceManager.getDefaultSharedPreferences(this).getString("authToken", "");
-			
-			runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					if(!isFetchingProfile){
-						new TrickyWordsTask(WordActivity.this, isAdding, WordActivity.this, WordActivity.this).run(params[0], params[1], newToken);
-					}
-					else {
-						new ProfileTask(WordActivity.this, WordActivity.this, WordActivity.this).run(params[0], newToken);
-						isFetchingProfile = false;
-					} 
-					Log.d(TAG, getString(R.string.token_error_retry));
-					Toast.makeText(WordActivity.this, getString(R.string.token_error_retry), Toast.LENGTH_SHORT).show();
-				}
-			});
-		}
-	}
-
-	@Override
-	public void onTrickyWord(Boolean success) {
-		if(success){
-			if(isAdding){
-				Toast.makeText(this, getString(R.string.success_added_word), Toast.LENGTH_SHORT).show();
-				btnAddTrickyWord.setText(getResources().getString(R.string.remove_tricky_word));
-				
-				trickyWords.add(currentWord);
-			}
-			else {
-				Toast.makeText(this, getString(R.string.success_removed_word), Toast.LENGTH_SHORT).show();
-				btnAddTrickyWord.setText(getResources().getString(R.string.add_tricky_word));
-				
-				if(currentIndex != -1)
-					trickyWords.remove(currentIndex);
-			}
-			//setWord(strWord);
-			
-		} else {
-			if(isAdding)
-				Toast.makeText(this, getString(R.string.failed_add_word), Toast.LENGTH_SHORT).show();
-			else
-				Toast.makeText(this, getString(R.string.failed_remove_word), Toast.LENGTH_SHORT).show();
-		}
-		
-		
-		// TODO: Change so server returns the word that has been added instead of "ok" and then remove this call to get profile information
-		btnOk.setEnabled(false);
-		String userId = Integer.toString(sp.getInt("id", 0));
-		String token = sp.getString("authToken", "");
-		isFetchingProfile = true;
-		new ProfileTask(this, this, this).run(userId, token);
-	}
-
-
-	@Override
-	public void onProfileFetched(String result) {
-		
-		UserProfile profile = new Gson().fromJson(result, UserProfile.class);
-		
-		trickyWords = (ArrayList<Word>) profile.getUserProblems().getTrickyWords();
-		
-		if(isAdding){
-			btnAddTrickyWord.setText(getResources().getString(R.string.remove_tricky_word));
-		}
-		else {
-			btnAddTrickyWord.setText(getResources().getString(R.string.add_tricky_word));
-		}
-		setWord(strWord);
-		btnOk.setEnabled(true);
-	}
-	
-	
+	}	
 }
